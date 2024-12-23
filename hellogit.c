d9d2ae86ea5e6c6fba4ae16f89af3dfc0447cf9c
Frits van Doorn <door6449@gmail.com>
2024-12-23 23:59:36 +0100

    Finding out how to get changes here locally into remote git repo.

--- hellogit.c
+++ hellogit.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 int main() {
     printf("Hello Frits form GIT with the latest version again.\n");
+    printf("This is 2024 12 23 23 58\n");
+    printf("See if we can get this in Github.\n");
     return 0;
 }
